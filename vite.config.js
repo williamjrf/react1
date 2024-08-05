3ddf7429026beeab11a136c09497c4dfaeb95eb6
William Rodriguez <williamjrf1@gmail.com>
2024-08-04 22:10:17 -0400

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg']
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
+  base:"https://williamjrf.github.io/react1/"
 })
